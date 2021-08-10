8fe92c42faeae1136d568faddaa7e4987cbe8516
Diego <mendizabal.diego@gmail.com>
2021-08-10 20:34:50 -0300

    Agregué vistas para crear y editar posteos

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns =[
     path('', views.lista_post, name = 'lista_post'),
     path('post/<int:pk>/', views.post_detalle, name='post_detalle'),
+    path('post/nuevo', views.post_nuevo, name='post_nuevo'),
+    path('post/<int:pk>/edit', views.post_editar, name='post_editar'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
 def lista_post(request):
-    posts = Post.objects.filter(fecha_publicado__lte=timezone.now()).order_by('fecha_publicado')
+    posts = Post.objects.filter(fecha_publicado__lte=timezone.now()).order_by('-fecha_publicado')
     return render(request, 'blog/lista_post.html', {
         'posts': posts
     })
@@ -13,3 +14,32 @@ def lista_post(request):
 def post_detalle(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detalle.html', {'post': post})
+
+def post_nuevo(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.fecha_publicado = timezone.now()
+            post.save()
+            return redirect('post_detalle', pk=post.pk)
+    else:
+        form = PostForm()
+    
+    return render(request, 'blog/post_editar.html', {'form': form})
+
+
+def post_editar(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.fecha_publicado = timezone.now()
+            post.save()
+            return redirect('post_detalle', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_editar.html', {'form': form})
